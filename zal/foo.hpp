077644c0055176bf39935ebaec651f9c5b4cb130
KrystianPi <38494173+KrystianPi@users.noreply.github.com>
2020-12-15 18:28:24 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -11,6 +11,7 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret_v(people.size());
 
     // Twoja implementacja tutaj
-
+    std::for_each(people.begin(), people.end(), []( Human& czlowiek){ czlowiek -> birthday();  })
+    std::transform(people.crbegin(), people.crend() , ret_v.begin, []( const Human& czlowiek){ return czlowiek -> isMonster()? 'n' : 'y' ;})
     return ret_v;
 }
