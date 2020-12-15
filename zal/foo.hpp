9c43764eb7c7757ba0abc71a15755fa10ed7541a
KrystianPi <38494173+KrystianPi@users.noreply.github.com>
2020-12-15 18:33:10 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -12,6 +12,6 @@ std::vector< char > foo(std::list< Human >& people)
 
     // Twoja implementacja tutaj
     std::for_each(people.begin(), people.end(), []( Human& czlowiek){ czlowiek.birthday();  });
-    std::transform(people.crbegin(), people.crend() , ret_v.begin, []( const Human& czlowiek){ return czlowiek.isMonster()? 'n' : 'y' ;});
+    std::transform(people.crbegin(), people.crend() , ret_v.begin(), []( const Human& czlowiek){ return czlowiek.isMonster()? 'n' : 'y' ;});
     return ret_v;
 }
